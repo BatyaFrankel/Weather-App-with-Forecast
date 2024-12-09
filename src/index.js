1c953ad22dba27f641fe9d5ef3e37eb88ad6d974
BatyaFrankel <frankelbatya@gmail.com>
2024-12-09 12:42:41 +0000

    used the api for data forecast

--- src/index.js
+++ src/index.js
@@ -62,24 +62,34 @@ function getForecast(city) {
   axios(apiUrl).then(displayForecast);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function displayForecast(responce) {
-  console.log(responce);
-  let days = ["Tomorrow", "Tuesday", "Wednesday", "Thursday", "Friday"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌦</div>
+  responce.data.daily.forEach(function (day, index) {
+    if (index > 0 && index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div class="weather-forecast-icon">
+            <img src="${day.condition.icon_url}" alt="weather icon"></div>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>12°C</strong>
+                <strong>${Math.round(day.temperature.maximum)}°C</strong>
               </div>
-              <div class="weather-forecast-temperature">5°C</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°C</div>
             </div>
           </div>`;
+    }
   });
 
   let forecast = document.querySelector("#forecast");
